0b633447ce8b279b619370420858410c28dc78fe
Luong Lit <pvluong0001@gmail.com>
2019-06-13 07:47:27 +0700

    06/13/2019 07:46:58 - update bash for seed

--- __setup/seed.sh
+++ __setup/seed.sh
@@ -4,3 +4,7 @@ php artisan db:seed --class=PermissionsTableSeeder
 php artisan db:seed --class=RoleHasPermissionsTableSeeder
 php artisan db:seed --class=ModelHasRolesTableSeeder
 php artisan db:seed --class=ModelHasPermissionsTableSeeder
+
+echo "User admin was created!"
+echo "User: viralsoft@vs.com"
+echo "Password: 123@123a"
