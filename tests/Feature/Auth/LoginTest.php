fced4141e3aca8ac044b4a5617a08315f1d3dcf8
Luong Lit <pvluong0001@gmail.com>
2019-06-17 09:11:46 +0700

    06/17/2019 09:11:13 - default test

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\BackpackUser;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/admin/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('backpack::auth.login');
+    }
+
+    public function test_user_can_login_with_correct_credentials() {
+        $user = factory(BackpackUser::class)->create([
+            'password' => bcrypt($password = self::PASSWD),
+        ]);
+        $response = $this->post('/admin/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+        $response->assertRedirect('/admin/dashboard');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_cannot_login_with_incorrect_password() {
+        $user = factory(BackpackUser::class)->create([
+            'password' => bcrypt(self::PASSWD),
+        ]);
+
+        $response = $this->from('/admin/login')->post('/admin/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertRedirect('/admin/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_user_can_logout() {
+        $user = factory(BackpackUser::class)->create([
+            'password' => bcrypt($password = self::PASSWD),
+        ]);
+        $response = $this->post('/admin/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+        $response->assertRedirect('/admin/dashboard');
+
+        $response = $this->actingAs($user)->from('/admin/dashboard')->get('/admin/logout');
+        $response->assertRedirect('/admin');
+    }
+}
