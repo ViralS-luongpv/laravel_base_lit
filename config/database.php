8c091847a07e238fc96488a0f9d8c95956d38e3a
Luong Lit <luongpv@viralsoft.vn>
2019-08-01 13:41:22 +0700

    08/01/2019 09:19:59 - update phpunit

--- config/database.php
+++ config/database.php
@@ -91,13 +91,14 @@ return [
             'prefix_indexes' => true,
         ],
 
+
         'testing_mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'testing',
+            'username' => 'root',
+            'password' => '123@123a',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
