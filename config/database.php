19f27a1ff54930c148900489ecfb6767d9d44723
Pham Luong <luongpv@@nlbn.net>
2019-08-01 09:20:19 +0700

    08/01/2019 09:19:59

--- config/database.php
+++ config/database.php
@@ -91,12 +91,24 @@ return [
             'prefix_indexes' => true,
         ],
 
-        'testing_sqlite' => [
-            'driver' => 'sqlite',
-            'database' => storage_path().'/testing.sqlite',
+        'testing_mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ]
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
     ],
 
     /*
